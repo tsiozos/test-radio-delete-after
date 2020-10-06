386df62edbb1aaa31f253560125f4b800bf13a4e
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-10-06 09:34:09 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,38 @@
+
+radio.set_group(49)
+radio.set_transmit_power(7)
+
+dat = bytearray(17)
+rcvd = bytearray(100)
+
+for i in range(16):
+    dat[i+1] = i
+for i in range(100):
+    rcvd[i] = 0
+
+
+def on_button_pressed_a():
+    global dat
+    for i in range(100):
+        dat[0]=i
+        radio.send_buffer(dat)
+        
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_a2():
+    global rcvd
+    tot = 0     #total
+    for i in range(100):
+        tot += rcvd[i]
+    print ("Received: "+str(tot)+serial.NEW_LINE)
+    print ("NOT received:"+serial.NEW_LINE)
+    for i in range(100):
+        if rcvd[i] == 0:
+            print(str(i)+serial.NEW_LINE)
+
+input.on_button_pressed(Button.B, on_button_pressed_a2)
+
+def on_received_buffer(receivedBuffer):
+    global rcvd
+    rcvd[receivedBuffer[0]] = 1
+radio.on_received_buffer(on_received_buffer)
---
+++ main.ts
@@ -0,0 +1,39 @@
+let i: number;
+radio.setGroup(49)
+radio.setTransmitPower(7)
+let dat = control.createBuffer(17)
+let rcvd = control.createBuffer(100)
+for (i = 0; i < 16; i++) {
+    dat[i + 1] = i
+}
+for (i = 0; i < 100; i++) {
+    rcvd[i] = 0
+}
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    for (let i = 0; i < 100; i++) {
+        dat[0] = i
+        radio.sendBuffer(dat)
+    }
+})
+input.onButtonPressed(Button.B, function on_button_pressed_a2() {
+    let i: number;
+    
+    let tot = 0
+    // total
+    for (i = 0; i < 100; i++) {
+        tot += rcvd[i]
+    }
+    console.log("Received: " + ("" + tot) + serial.NEW_LINE)
+    console.log("NOT received:" + serial.NEW_LINE)
+    for (i = 0; i < 100; i++) {
+        if (rcvd[i] == 0) {
+            console.log("" + i + serial.NEW_LINE)
+        }
+        
+    }
+})
+radio.onReceivedBuffer(function on_received_buffer(receivedBuffer: Buffer) {
+    
+    rcvd[receivedBuffer[0]] = 1
+})
